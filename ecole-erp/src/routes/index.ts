d236e26b9f4681aefe6afa83c2a016cb6c816cd8
lkoatedja2-rgb <lkoatedja2@gmail.com>
2026-08-12 22:41:59 +0000

    Add new routers for notes, personnel, and more

--- ecole-erp/src/routes/index.ts
+++ ecole-erp/src/routes/index.ts
@@ -9,11 +9,35 @@ import { inscriptionsRouter } from "@/modules/scolarite/inscriptions/inscription
 import { absencesRouter } from "@/modules/vie-scolaire/absences/absences.routes";
 import { matieresRouter } from "@/modules/pedagogie/matieres/matieres.routes";
 import { coursRouter } from "@/modules/pedagogie/cours/cours.routes";
-import { not
+import { notesRouter } from "@/modules/pedagogie/notes/notes.routes";
+import { personnelRouter } from "@/modules/rh/personnel/personnel.routes";
+import { contratsRouter } from "@/modules/rh/contrats/contrats.routes";
+import { bulletinsPaieRouter } from "@/modules/rh/bulletins-paie/bulletins-paie.routes";
+import { facturesRouter } from "@/modules/comptabilite/factures/factures.routes";
+import { sallesRouter } from "@/modules/edt/salles/salles.routes";
+import { creneauxRouter } from "@/modules/edt/creneaux/creneaux.routes";
 
-// Scolarité
+export const apiRouter = Router();
+
+apiRouter.use("/auth", authRouter);
+
 apiRouter.use("/etablissements", etablissementsRouter);
 apiRouter.use("/annees-scolaires", anneesScolairesRouter);
 apiRouter.use("/classes", classesRouter);
 apiRouter.use("/eleves", elevesRouter);
-apiRouter.use("/inscripti
+apiRouter.use("/inscriptions", inscriptionsRouter);
+
+apiRouter.use("/absences", absencesRouter);
+
+apiRouter.use("/matieres", matieresRouter);
+apiRouter.use("/cours", coursRouter);
+apiRouter.use("/notes", notesRouter);
+
+apiRouter.use("/personnel", personnelRouter);
+apiRouter.use("/contrats", contratsRouter);
+apiRouter.use("/bulletins-paie", bulletinsPaieRouter);
+
+apiRouter.use("/factures", facturesRouter);
+
+apiRouter.use("/salles", sallesRouter);
+apiRouter.use("/creneaux-edt", creneauxRouter);
