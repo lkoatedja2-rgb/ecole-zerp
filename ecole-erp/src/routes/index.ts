57f636236923f6e69f3a8670ad198866390b49d6
lkoatedja2-rgb <lkoatedja2@gmail.com>
2026-08-12 22:04:56 +0000

    Add routes for salles and creneaux

--- ecole-erp/src/routes/index.ts
+++ ecole-erp/src/routes/index.ts
@@ -14,36 +14,30 @@ import { personnelRouter } from "@/modules/rh/personnel/personnel.routes";
 import { contratsRouter } from "@/modules/rh/contrats/contrats.routes";
 import { bulletinsPaieRouter } from "@/modules/rh/bulletins-paie/bulletins-paie.routes";
 import { facturesRouter } from "@/modules/comptabilite/factures/factures.routes";
+import { sallesRouter } from "@/modules/edt/salles/salles.routes";
+import { creneauxRouter } from "@/modules/edt/creneaux/creneaux.routes";
 
 export const apiRouter = Router();
 
-// Identité / Auth
 apiRouter.use("/auth", authRouter);
 
-// Scolarité
 apiRouter.use("/etablissements", etablissementsRouter);
 apiRouter.use("/annees-scolaires", anneesScolairesRouter);
 apiRouter.use("/classes", classesRouter);
 apiRouter.use("/eleves", elevesRouter);
 apiRouter.use("/inscriptions", inscriptionsRouter);
 
-// Vie scolaire
 apiRouter.use("/absences", absencesRouter);
 
-// Pédagogie
 apiRouter.use("/matieres", matieresRouter);
 apiRouter.use("/cours", coursRouter);
 apiRouter.use("/notes", notesRouter);
 
-// RH
 apiRouter.use("/personnel", personnelRouter);
 apiRouter.use("/contrats", contratsRouter);
 apiRouter.use("/bulletins-paie", bulletinsPaieRouter);
 
-// Comptabilité
 apiRouter.use("/factures", facturesRouter);
 
-// TODO (prochaines itérations, même pattern module):
-// - /modules/communication  (messagerie interne, notifications)
-// - /modules/edt            (emploi du temps, salles - CreneauEdt existe déjà en DB)
-// - /modules/vie-scolaire/sanctions
+apiRouter.use("/salles", sallesRouter);
+apiRouter.use("/creneaux-edt", creneauxRouter);
